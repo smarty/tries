263c2a0eff1c464e639610e7c91ea74f296a9c8a
Timothy Eckstein <timothy@smarty.com>
2026-01-06 10:41:30 -0700

    bad code

--- errors.go
+++ errors.go
@@ -2,6 +2,8 @@ package tries
 
 import "errors"
 
+// ! important ! !!! Move this to contracts.go when done editing and before commit !!!
+
 var (
 	ErrorBadTrieKey = errors.New("unable to create Trie with bad key type")
 )
